033df3bdc1d8e4b3bbd2235c090341956140ed8d
Marquet Reid <you@example.com>
2018-04-05 03:13:04 -0400

    changed header to navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <nav>
+        <div class="nav-wrapper blue darken-4">
+            <a href="/" class="brand-logo">Keto Recipes</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="/">Home</a></li>
+                <li><a href="/#/about">About</a></li>
+                <li><a href="/#/recipes">Recipe Index</a></li>
+                <li><a href="/#/recipes/add-recipe">Add Recipe</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+    color: teal;
+} 
+nav {
+    margin-top: -9vh;
+}
+.brand-logo {
+    color: teal;
+}
+</style>
